731ecc7b018ce7a689bd79804625cce63627f312
margintradings777-sudo <margintradings777@gmail.com>
2026-01-18 16:53:49 +0530

    Add files via upload

---
+++ src/components/footer.jsx
@@ -0,0 +1,86 @@
+const Footer = () => {
+    return (
+        <footer class="footer">
+        <div class="footer-top">
+          <div class="social-media">
+            <strong>FBS at social media</strong>
+            <div class="social-icons">
+              <i class="fab fa-facebook-f"></i>
+              <i class="fab fa-instagram"></i>
+              <i class="fab fa-youtube"></i>
+              <i class="fab fa-telegram-plane"></i>
+            </div>
+          </div>
+          <div class="contact-us">
+            <strong>Contact us</strong>
+            <div class="contact-icons">
+              <i class="fas fa-envelope"></i>
+              <i class="fab fa-telegram-plane"></i>
+              <i class="fab fa-whatsapp"></i>
+            </div>
+          </div>
+          <a href="#" class="google-play-btn">
+            <i class="fab fa-google-play"></i> Get on the Google Play
+          </a>
+        </div>
+
+        <div class="footer-links">
+          <div>
+            <h4>Trading</h4>
+            <a href="#">Trading conditions</a>
+            <a href="#">Deposits & withdrawals</a>
+            <a href="#">FBS app</a>
+            <a href="#">MetaTrader 5</a>
+            <a href="#">MetaTrader 4</a>
+            <a href="#">Open a Forex account</a>
+          </div>
+          <div>
+            <h4>Market</h4>
+            <a href="#">Forex</a>
+            <a href="#">Metals</a>
+            <a href="#">Indices</a>
+            <a href="#">Energies</a>
+            <a href="#">Stocks</a>
+            <a href="#">Forex Exotic</a>
+          </div>
+          <div>
+            <h4>Tools</h4>
+            <a href="#">Economic calendar</a>
+            <a href="#">Trading calculators</a>
+            <a href="#">VPS</a>
+          </div>
+          <div>
+            <h4>Analytics</h4>
+            <a href="#">Market Analytics</a>
+            <a href="#">VIP Analytics</a>
+            <a href="#">Top trades of the hour</a>
+          </div>
+          <div>
+            <h4>Education</h4>
+            <a href="#">FBS Academy</a>
+            <a href="#">Trader's blog</a>
+            <a href="#">Glossary</a>
+          </div>
+          <div>
+            <h4>Company</h4>
+            <a href="#">About FBS</a>
+            <a href="#">Legal documents</a>
+            <a href="#">Company news</a>
+            <a href="#">Career at FBS</a>
+            <a href="#">FC Leicester City</a>
+            <a href="#">Live Chat</a>
+            <a href="#">Callback</a>
+            <a href="#">Help Center</a>
+          </div>
+          <div>
+            <h4>Address</h4>
+            <p>Sky City at Borivali, E Block 808,</p>
+            <p>Borivali West, Mumbai, Maharashtra,</p>
+            <p>Pin Code: 400092, India.</p>
+          </div>
+        </div>
+      </footer>
+    )
+}
+
+export default Footer
