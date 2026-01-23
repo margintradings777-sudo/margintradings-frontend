0dcc4a6c3dec4c6044ed2a44208fbf48e37ec555
margintradings777-sudo <margintradings777@gmail.com>
2026-01-23 17:53:45 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,17 @@
 import React from 'react'
-import {HashRouter,Routes, Route} from "react-router"
+import { HashRouter, Routes, Route } from "react-router-dom"
 import Home from "./Home.jsx"
 import Profile from "./Profile.jsx"
 
 function App() {
   return (
- <HashRouter>
-  <Routes>
-    <Route exact path="" element={<Home/>}/>
-    <Route path="/profile" element={<Profile/>}/>
-  </Routes>
- </HashRouter>
+    <HashRouter>
+      <Routes>
+      
+        <Route path="/" element={<Home />} /> 
+        <Route path="/profile" element={<Profile />} />
+      </Routes>
+    </HashRouter>
   )
 }
 
